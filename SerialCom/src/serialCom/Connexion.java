4f81cc7d0047631908dbed8fa6531498a18edd47
Lightel24 <Lightel24@gmail.com>
2019-07-24 17:21:06 +0200

    Création de la classe Connection
    Pour permettre de gerer des connexions multiples

---
+++ SerialCom/src/serialCom/Connexion.java
@@ -0,0 +1,164 @@
+package serialCom;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+
+import main.Fenetre;
+
+public class Connexion {
+
+	protected SerialPort portCom;
+	protected SerialListener listener;
+	protected SerialWriter writer;
+	protected Thread listenerThread;
+	protected Thread writerThread;
+	static boolean Running;
+	
+	public boolean connect(int nbr){
+		System.out.println("Recherche du port: COM"+nbr);
+		SerialPort[] ports = SerialPort.getCommPorts();
+		String[] noms = ConnexionManager.getAvailiblePortNames();
+		for(int i=0; i<ports.length;i++) {
+			System.out.println("Port detecte: " + noms[i]);
+			if(noms[i].contains("COM"+nbr)) {
+				portCom = ports[i];
+				System.out.println("Port COM"+nbr+" obtenu.\n Ouverture");
+				break;
+			}
+		}
+		if(portCom!= null && portCom.openPort()) {
+			System.out.println("Connexion établie avec succès!");
+			init();
+			return true;
+		}else {
+			System.err.println("Erreur la connexion n'a pas été établie.");
+			return false;
+		}
+	}
+	
+	public boolean connect(String nom){
+		System.out.println("Recherche du port: "+nom);
+		SerialPort[] ports = SerialPort.getCommPorts();
+		String[] noms = ConnexionManager.getAvailiblePortNames();
+		for(int i=0; i<ports.length;i++) {
+			System.out.println("Port detecte: " + noms[i]);
+			if(noms[i].contains(nom)) {
+				portCom = ports[i];
+				System.out.println("Port: "+nom+" obtenu.\n Ouverture");
+				break;
+			}
+		}
+		if(portCom!= null && portCom.openPort()) {
+			System.out.println("Connexion établie avec succès!");
+			init();
+			return true;
+		}else {
+			System.err.println("Erreur la connexion n'a pas été établie.");
+			return false;
+		}
+	}
+	
+	private void init() {
+		System.out.println("Initialisation du Writer et du Listener...");		
+		Running = true;
+		listener = new SerialListener();
+		writer = new SerialWriter();
+		listenerThread = new Thread(listener);
+		listenerThread.start();
+		
+		writerThread = new Thread(writer);
+		writerThread.start();
+	}
+	
+	public void send(String message) {
+		writer.send(message);
+	}
+
+	public boolean close() {
+		if(portCom.closePort()) {
+			Running = false;
+			try {
+				listenerThread.join();
+				writerThread.join();
+				return true;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+	public void addDataListener(SerialPortDataListener listener) {
+		if(Running) {
+			portCom.addDataListener(listener);
+		}
+	}
+	
+	public void waitForAnswer(String message) {
+		listener.waitForAnswer(message);
+	}
+	
+	
+
+	private class SerialListener implements Runnable{
+		private volatile String toWait = "";
+
+		@Override
+		public synchronized  void run() {
+			String stringBuffer = "";
+			while(Running) {
+				if(portCom.bytesAvailable()>1) {
+					byte[] buffer = new byte[portCom.bytesAvailable()];
+					portCom.readBytes(buffer, buffer.length);
+					stringBuffer = new String(buffer);
+					}
+				
+					if(!stringBuffer.isEmpty()) {
+						Fenetre.addText(portCom.getSystemPortName()+":  "+stringBuffer);	
+						stringBuffer= stringBuffer.replaceAll("\r", "");
+						stringBuffer= stringBuffer.replaceAll("\n", "");
+						stringBuffer= stringBuffer.replaceAll("\t", "");
+						if(toWait.equals(stringBuffer)) {
+							synchronized (toWait) {
+								toWait.notifyAll();
+							}
+						}
+					stringBuffer = "";
+					}
+			}
+		}
+
+		public synchronized void waitForAnswer(String message) {
+			toWait = message;
+			try {
+				synchronized (toWait) {
+					toWait.wait();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}	
+	}
+	
+		
+	
+	private class SerialWriter implements Runnable{
+		private volatile String buffer = "";
+
+		@Override
+		public void run() {
+			while(Running) {
+				if(buffer!="") {
+					portCom.writeBytes(buffer.getBytes(), buffer.getBytes().length);
+					Fenetre.addText("SENT: "+buffer+"\n");
+					buffer = "";
+				}
+			}	
+		}
+		
+		public void send(String message) {
+			buffer += message;
+		}
+	
+	}
+}
--- SerialCom/src/serialCom/ConnexionManager.java
+++ SerialCom/src/serialCom/ConnexionManager.java
@@ -1,63 +1,41 @@
 package serialCom;
 
+import java.util.HashMap;
+import java.util.Set;
+
 import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 
-import main.Fenetre;
-
 public abstract class ConnexionManager {
+	private static HashMap<Object,Connexion> Connections = new HashMap<Object,Connexion>();
 	
-	protected static SerialPort portCom;
-	protected static SerialListener listener;
-	protected static SerialWriter writer;
-	protected static Thread listenerThread;
-	protected static Thread writerThread;
-	static boolean Running;
 	
-	public static boolean connect(int nbr){
-		System.out.println("Recherche du port: COM"+nbr);
-		SerialPort[] ports = SerialPort.getCommPorts();
-		String[] noms = getAvailiblePortNames();
-		for(int i=0; i<ports.length;i++) {
-			System.out.println("Port detecte: " + noms[i]);
-			if(noms[i].contains("COM"+nbr)) {
-				portCom = ports[i];
-				System.out.println("Port COM"+nbr+" obtenu.\n Ouverture");
-				break;
-			}
+	public static Object connect(int nbr){
+		Connexion nouv = new Connexion();
+		if(nouv.connect(nbr)) {
+			Object key = generateID();
+			Connections.put(key,nouv);
+			return key;
 		}
-		if(portCom!= null && portCom.openPort()) {
-			System.out.println("Connexion établie avec succès!");
-			init();
-			return true;
-		}else {
-			System.err.println("Erreur la connexion n'a pas été établie.");
-			return false;
-		}
+		return null;
 	}
 	
-	public static boolean connect(String nom){
-		System.out.println("Recherche du port: "+nom);
-		SerialPort[] ports = SerialPort.getCommPorts();
-		String[] noms = getAvailiblePortNames();
-		for(int i=0; i<ports.length;i++) {
-			System.out.println("Port detecte: " + noms[i]);
-			if(noms[i].contains(nom)) {
-				portCom = ports[i];
-				System.out.println("Port: "+nom+" obtenu.\n Ouverture");
-				break;
-			}
+	
+	public static Object connect(String nom){
+		Connexion nouv = new Connexion();
+		if( nouv.connect(nom)) {
+			Object key = generateID();
+			Connections.put(key,nouv);
+			return key;
 		}
-		if(portCom!= null && portCom.openPort()) {
-			System.out.println("Connexion établie avec succès!");
-			init();
-			return true;
-		}else {
-			System.err.println("Erreur la connexion n'a pas été établie.");
-			return false;
-		}
+		return null;
 	}
 	
+	private static Object generateID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	public static String[] getAvailiblePortNames() {
 		SerialPort[] ports = SerialPort.getCommPorts();
 		String[] names = new String[ports.length];
@@ -68,106 +46,34 @@ public abstract class ConnexionManager {
 	}
 	
 	private static void init() {
-		System.out.println("Initialisation du Writer et du Listener...");		
-		Running = true;
-		listener = new SerialListener();
-		writer = new SerialWriter();
-		listenerThread = new Thread(listener);
-		listenerThread.start();
 		
-		writerThread = new Thread(writer);
-		writerThread.start();
 	}
 	
 	public static void send(String message) {
-		SerialWriter.send(message);
+
 	}
 
-	public static boolean close() {
-		if(portCom.closePort()) {
-			Running = false;
-			try {
-				listenerThread.join();
-				writerThread.join();
-				return true;
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+	public static boolean close(Object key) {
+		return Connections.get(key).close();
+	}
+	
+	public static boolean closeAll() {
+		Set<Object> keys = Connections.keySet();
+		for(Object key: keys) {
+			close(key);
 		}
-		return false;
+		return false;	//TODO
 	}
 	
 	public static void addDataListener(SerialPortDataListener listener) {
-		if(Running) {
-			portCom.addDataListener(listener);
-		}
+		
 	}
 	
 	public static void waitForAnswer(String message) {
-		SerialListener.waitForAnswer(message);
+
 	}
 	
 	
 
-	private static class SerialListener implements Runnable{
-		private static volatile String toWait = "";
-
-		@Override
-		public synchronized  void run() {
-			String stringBuffer = "";
-			while(Running) {
-				if(portCom.bytesAvailable()>1) {
-					byte[] buffer = new byte[portCom.bytesAvailable()];
-					portCom.readBytes(buffer, buffer.length);
-					stringBuffer = new String(buffer);
-					}
-				
-					if(!stringBuffer.isEmpty()) {
-						Fenetre.addText(portCom.getSystemPortName()+":  "+stringBuffer);	
-						stringBuffer= stringBuffer.replaceAll("\r", "");
-						stringBuffer= stringBuffer.replaceAll("\n", "");
-						stringBuffer= stringBuffer.replaceAll("\t", "");
-						if(toWait.equals(stringBuffer)) {
-							synchronized (toWait) {
-								toWait.notifyAll();
-							}
-						}
-					stringBuffer = "";
-					}
-			}
-		}
-
-		public synchronized static void waitForAnswer(String message) {
-			toWait = message;
-			try {
-				synchronized (toWait) {
-					toWait.wait();
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}	
-	}
 	
-		
-	
-	private static class SerialWriter implements Runnable{
-		private static volatile String buffer = "";
-
-		@Override
-		public void run() {
-			while(Running) {
-				if(buffer!="") {
-					portCom.writeBytes(buffer.getBytes(), buffer.getBytes().length);
-					Fenetre.addText("SENT: "+buffer+"\n");
-					buffer = "";
-				}
-			}	
-		}
-		
-		public static void send(String message) {
-			buffer += message;
-		}
-	
-	}
 }
