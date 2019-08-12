f55c7429b1266226ea6cd9b718055374d490d7e4
Lightel24 <Lightel24@gmail.com>
2019-08-12 09:52:28 +0200

    Changement de GUI
    -Autoscroll a gerer

--- SerialCom/src/main/Fenetre.java
+++ SerialCom/src/main/Fenetre.java
@@ -3,47 +3,60 @@ package main;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 import serialCom.ConnexionManager;
 import serialCom.DagoPrint;
 
 public class Fenetre extends JFrame{
 	
-	private JSplitPane JSPmain;	
+	private JSplitPane splitPane;	
 	protected JFrame ref = this;
 	
-	private JPanel cons = new JPanel();
-	private JPanel other = new JPanel();
-	
-	private static JTextArea JTAcons = new JTextArea();
-	private JScrollPane scroll = new JScrollPane(JTAcons);
-	private JPanel JPsend = new JPanel();
-	private JTextField JTFenter = new JTextField();
-	private JButton JBsend = new JButton("Envoyer");
+	private JPanel Gauche;
+	private JPanel Droite;
+	private JPanel Liste;
+
+	private static JTextArea JTAcons;
+	private JScrollPane scroll;
+	private JScrollPane scrollPane;
+	private JCheckBox AutoScroll;
+	private JPanel JPsend;
+	private JTextField JTFenter;
+	private JButton JBsend;
 	
 	
 	private JMenuBar menuBar = new JMenuBar();
@@ -55,44 +68,96 @@ public class Fenetre extends JFrame{
 	
 	public String currentConnection;
 	
+	
+	
+	
+	
 	public Fenetre() {
-		String[] ports;
-		do {
-			ports = ConnexionManager.getAvailiblePortNames();
-			if(ports.length<=0) {
-				int rep = JOptionPane.showConfirmDialog(ref, "Pas de port actif detectés", "Erreur", JOptionPane.CANCEL_OPTION,JOptionPane.ERROR_MESSAGE);
-				if(rep!=JOptionPane.OK_OPTION) {
-					System.exit(0);
-				}
+		initialize();
+		this.setVisible(true);
+	}
+	
+	
+	private void initialize() {
+		String[] ports = ConnexionManager.getAvailiblePortNames();
+		if(ports.length<=0) {
+			int rep = JOptionPane.showConfirmDialog(ref, "Pas de port série actif detectés", "Erreur", JOptionPane.CANCEL_OPTION,JOptionPane.ERROR_MESSAGE);
+			if(rep!=JOptionPane.OK_OPTION) {
+				System.exit(0);
 			}
-		}while(ports.length<=0);
+		}
 		
- 		
-//		ports = new String[]{"COM3","COM4"};	//DEBUG
-		
-		//Parametres de la JFrame
-		this.setTitle("SerialCom");
-		this.setSize(1000, 700);
-		this.setLocationRelativeTo(null);
+		//ports = new String[]{"COM3","COM4"};	//DEBUG
+			
+		ref = this;
+		this.setTitle("MultiCom");
+		this.setBounds(100, 100, 972, 517);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		other.setLayout(new GridLayout(ports.length,1));
+		this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.X_AXIS));
 		
-		    ButtonGroup bg = new ButtonGroup();
+		splitPane = new JSplitPane();
+		this.getContentPane().add(splitPane);
+		
+		Gauche = new JPanel();
+		Gauche.setBackground(Color.DARK_GRAY);
+		splitPane.setLeftComponent(Gauche);
+		Gauche.setLayout(new BoxLayout(Gauche, BoxLayout.X_AXIS));
+		
+		scrollPane = new JScrollPane();
+		Gauche.add(scrollPane);
+		
+		Liste = new JPanel();
+		Liste.setBorder(new EmptyBorder(10, 10, 10, 10));
+		Liste.setBounds(new Rectangle(10, 10, 10, 10));
+		Liste.setBackground(Color.DARK_GRAY);
+		scrollPane.setViewportView(Liste);
+		
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+		Liste.setLayout(new BoxLayout(Liste, BoxLayout.PAGE_AXIS));
+		
+		/*Les multiples connections */
+		ButtonGroup bg = new ButtonGroup();
 		//Création des multiples UI de connection
 		for(int i=0;i<ports.length;i++) {
 			JRadioButton select = new JRadioButton("Selectionné");
 			SerialConnectionPanel panel = new SerialConnectionPanel(ports[i],select);
 			bg.add(select);
-	        other.add(panel);
-		}
+			Liste.add(panel);
+			Component verticalStrut_1 = Box.createHorizontalStrut(3);
+			verticalStrut_1.setMaximumSize(new Dimension(0, 3));
+			Liste.add(verticalStrut_1);
+		}		
+		/*Fin*/
 		
 		
+		Droite = new JPanel();
+		splitPane.setRightComponent(Droite);
+		Droite.setLayout(new BorderLayout(0, 0));
 		
-		//Ajout et gestion contenu JPanel cons -> ensemble console
+		scroll = new JScrollPane();
+		Droite.add(scroll, BorderLayout.CENTER);
+		
+		JTAcons = new JTextArea();
+		scroll.setViewportView(JTAcons);
 		JTAcons.setEditable(false);
-		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
+		JPanel panel = new JPanel();
+		Droite.add(panel, BorderLayout.SOUTH);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		AutoScroll = new JCheckBox("Autoscroll");
+		panel.add(AutoScroll, BorderLayout.WEST);
+		
+		JTFenter = new JTextField();
+		panel.add(JTFenter, BorderLayout.CENTER);
+		JTFenter.setColumns(10);
+		
+		JBsend = new JButton("Envoyer");
+		panel.add(JBsend, BorderLayout.EAST);
+		
+		/*Actions Listeners*/
+		//Bouton envoyer
 		JBsend.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -100,6 +165,7 @@ public class Fenetre extends JFrame{
 			}		
 		});
 		
+		//Appuie sur la touche entrée
 		JTFenter.addKeyListener(new KeyListener() {
 
 			@Override
@@ -116,16 +182,6 @@ public class Fenetre extends JFrame{
 			public void keyTyped(KeyEvent arg0) {}
 			
 		});
-		/*
-		 * Ajout composants console aux JPanels
-		 */
-		JPsend.setLayout(new BorderLayout());
-		JPsend.add(JTFenter, BorderLayout.CENTER);
-		JPsend.add(JBsend, BorderLayout.LINE_END);	
-		cons.setLayout(new BorderLayout());
-		cons.add(scroll, BorderLayout.CENTER);
-		cons.add(JPsend, BorderLayout.SOUTH);
-		
 		
 		/*
 		 * Gestion JMenuBar
@@ -151,18 +207,10 @@ public class Fenetre extends JFrame{
 		
 		menuBar.add(JMoption);
 		menuBar.add(JMpropos);
-		
-		/*
-		 * Gestion JFrame avec spliter
-		 */
-		//Definition paramètre Spliter
-		JSPmain = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, other, cons);
-		JSPmain.setDividerLocation(675);
-		//Ajout du tout dans la JFrame
-		this.add(JSPmain);
 		this.setJMenuBar(menuBar);
-		this.setVisible(true);
 	}
+	
+	
 	
 	protected String getSelectedConnectionName() {
 		return currentConnection;
@@ -183,7 +231,7 @@ public class Fenetre extends JFrame{
 	}
 	
 	public void updateAllConectionPanel() {
-		Component[] comp = other.getComponents();
+		Component[] comp = Liste.getComponents();
 		
 		for(Component panel:comp) {
 			if(panel instanceof SerialConnectionPanel) {
@@ -213,12 +261,35 @@ public class Fenetre extends JFrame{
 		
 		public SerialConnectionPanel(String port, JRadioButton select) {
 			
-			JCBcom = new JLabel(port);
-			JBcon = new JButton("Connexion");
 			
-			/*Création du JDropDownButton*/
+			this.setBorder(new EmptyBorder(10, 10, 10, 10));
+			this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+			this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+			this.add(select);
+			
+			Component horizontalStrut = Box.createHorizontalStrut(10);
+			horizontalStrut.setMaximumSize(new Dimension(20, 0));
+			this.add(horizontalStrut);
+			
+			JCBcom = new JLabel(port);
+			JCBcom.setBorder(new EmptyBorder(0, 0, 0, 0));
+			JCBcom.setOpaque(true);
+			this.add(JCBcom);
+			
+			Component horizontalStrut_1 = Box.createHorizontalStrut(10);
+			horizontalStrut_1.setMaximumSize(new Dimension(20, 0));
+			this.add(horizontalStrut_1);
+			
+			JBcon = new JButton("Connection");
+			JBcon.setBackground(new Color(255, 228, 181));
+			JBcon.setBorderPainted(false);
+			this.add(JBcon);
+			
+
+			/*Création du JComboBox*/
 			JMenuItem dagoPrint = new JMenuItem("Impression GCode");
 			JDDBactions = new JComboBox(actions);
+			JDDBactions.setMaximumSize(new Dimension(32767, 20));
 			JDDBactions.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
@@ -237,6 +308,7 @@ public class Fenetre extends JFrame{
 			JDDBactions.add(dagoPrint);
 			JDDBactions.setEnabled(false);
 			JBcon.setEnabled(false);
+			this.add(JDDBactions);
 
 			/*Selection des comm*/
 			select.addActionListener(new ActionListener() { 
@@ -250,41 +322,31 @@ public class Fenetre extends JFrame{
 				}	
 			});
 			
-			/*
-			 * Ajout et gestion contenu JPanel other -> le reste de la JFrame (a définir)
-			 */
-			//1er partie : connexion imprimante
+			//Connexion
 			
-				JBcon.addActionListener(new ActionListener() { //Bouton connexion/deconnexion --> connect a l'imprimante
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						
-						if(stateJBcon){ //true = boutton affiche connexion
-							if(ConnexionManager.connectSerial(JCBcom.getText())) {
-								JBcon.setText("Deconnexion");
-								stateJBcon = !stateJBcon;
-							}else{
-								JOptionPane.showMessageDialog(null, "Une erreur est survenu lors de la connexion. Esseyez ultèrieurement", "Erreur", JOptionPane .ERROR_MESSAGE);
-							}
-							
+			JBcon.addActionListener(new ActionListener() { //Bouton connexion/deconnexion --> connect a l'imprimante
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					
+					if(stateJBcon){ //true = boutton affiche connexion
+						if(ConnexionManager.connectSerial(JCBcom.getText())) {
+							JBcon.setText("Deconnexion");
+							stateJBcon = !stateJBcon;
 						}else{
-							if(ConnexionManager.close(JCBcom.getText())) {
-								JBcon.setText("Connexion");
-								stateJBcon = !stateJBcon;
-							}else{
-								JOptionPane.showMessageDialog(null, "Une erreur est survenu lors de la déconnexion. Esseyez ultèrieurement", "Erreur", JOptionPane .ERROR_MESSAGE);
-							}
+							JOptionPane.showMessageDialog(null, "Une erreur est survenu lors de la connexion. Esseyez ultèrieurement", "Erreur", JOptionPane .ERROR_MESSAGE);
 						}
-						JDDBactions.setEnabled(!stateJBcon);
-					}			
-				});
-			
-				
-			this.add(select);
-			this.add(JCBcom);
-			this.add(JBcon);
-			this.add(JDDBactions);
-			this.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+						
+					}else{
+						if(ConnexionManager.close(JCBcom.getText())) {
+							JBcon.setText("Connexion");
+							stateJBcon = !stateJBcon;
+						}else{
+							JOptionPane.showMessageDialog(null, "Une erreur est survenu lors de la déconnexion. Esseyez ultèrieurement", "Erreur", JOptionPane .ERROR_MESSAGE);
+						}
+					}
+					JDDBactions.setEnabled(!stateJBcon);
+				}			
+			});
 		}
 
 		public void updateButtonState(boolean state) {
