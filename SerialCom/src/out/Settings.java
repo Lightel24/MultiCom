839fc4bd283f24f41b83fbb38741d9b7fee29a62
Lightel24 <Lightel24@gmail.com>
2019-08-25 00:35:35 +0200

    Ajout d'option du choix de delai

--- SerialCom/src/out/Settings.java
+++ SerialCom/src/out/Settings.java
@@ -12,6 +12,8 @@ public final class Settings {
 	
 	private static boolean DEFAULT_PING = true;
 	private static boolean PING;
+	private static int DELAY;
+	private static int DEFAULT_DELAY = 150;
 	
 	private static File fichier = new File("options.txt");
 	
@@ -22,11 +24,19 @@ public final class Settings {
 	public static boolean isPing() {
 		return PING;
 	}
+	
+	public static void setDelay(int delay) {
+		DELAY = delay;
+	}
+	
+	public static int getDelay() {
+		return DELAY;
+	}
 
 	public static void checkSettings() {
+		reset();
 		if(!fichier.exists()) {
 			System.out.println("Création du fichier de configuration...");
-			reset();
 			saveSettings();
 		}else {
 			System.out.println("Lecture du fichier de configuration...");
@@ -36,6 +46,7 @@ public final class Settings {
 
 	private static void reset() {
 		PING = DEFAULT_PING;
+		DELAY = DEFAULT_DELAY;
 	}
 
 	private static void loadSettings() {
@@ -44,13 +55,23 @@ public final class Settings {
 			br = new BufferedReader(new FileReader(fichier));
 			String line;
 			while((line = br.readLine()) != null){
-				/*Pour le PING */
-				if(line.substring(0, line.indexOf('=')).equals("PING")) {
-					if(line.substring(line.indexOf('=')+1).equals("true")) {
-						PING = true;
-					}else {
-						PING = false;
-					}
+				System.out.println(line);
+				switch(line.substring(0, line.indexOf('='))) {
+					case "PING":
+						if(line.substring(line.indexOf('=')+1).equals("true")) {
+							PING = true;
+						}else {
+							PING = false;
+						}
+					break;
+					
+					case "DELAY":
+						try {
+							DELAY = Integer.parseInt(line.substring(line.indexOf('=')+1));
+						}catch(NumberFormatException e) {
+							e.printStackTrace();
+						}
+					break;
 				}
 			}
 		} catch (FileNotFoundException e) {
@@ -75,6 +96,10 @@ public final class Settings {
 			 /*-------Enregistrement du ping----------*/
 			 bw.write("PING="+PING + "\n");
 			 System.out.print("PING="+PING + "\n");
+			 
+			 /*-------Enregistrement du délai----------*/
+			 bw.write("DELAY="+DELAY + "\n");
+			 System.out.print("DELAY="+DELAY + "\n");
 
 		} catch (IOException e) {
 			e.printStackTrace();
