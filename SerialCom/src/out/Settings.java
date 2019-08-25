da571c282912fc34c0f12227bbd3576b4b074d95
Lightel24 <Lightel24@gmail.com>
2019-08-25 12:26:01 +0200

    MultiTHreading pour les I/O des parametres

--- SerialCom/src/out/Settings.java
+++ SerialCom/src/out/Settings.java
@@ -39,7 +39,6 @@ public final class Settings {
 			System.out.println("Création du fichier de configuration...");
 			saveSettings();
 		}else {
-			System.out.println("Lecture du fichier de configuration...");
 			loadSettings();
 		}
 	}
@@ -50,67 +49,79 @@ public final class Settings {
 	}
 
 	private static void loadSettings() {
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(fichier));
-			String line;
-			while((line = br.readLine()) != null){
-				System.out.println(line);
-				switch(line.substring(0, line.indexOf('='))) {
-					case "PING":
-						if(line.substring(line.indexOf('=')+1).equals("true")) {
-							PING = true;
-						}else {
-							PING = false;
+		new Thread() {
+			@Override
+			public void run() {
+				System.out.println("Lecture du fichier de configuration...");
+				BufferedReader br = null;
+				try {
+					br = new BufferedReader(new FileReader(fichier));
+					String line;
+					while((line = br.readLine()) != null){
+						System.out.println(line);
+						switch(line.substring(0, line.indexOf('='))) {
+							case "PING":
+								if(line.substring(line.indexOf('=')+1).equals("true")) {
+									PING = true;
+								}else {
+									PING = false;
+								}
+							break;
+							
+							case "DELAY":
+								try {
+									DELAY = Integer.parseInt(line.substring(line.indexOf('=')+1));
+								}catch(NumberFormatException e) {
+									e.printStackTrace();
+								}
+							break;
 						}
-					break;
-					
-					case "DELAY":
-						try {
-							DELAY = Integer.parseInt(line.substring(line.indexOf('=')+1));
-						}catch(NumberFormatException e) {
-							e.printStackTrace();
-						}
-					break;
+					}
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}finally {
+					try {
+						br.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
+				System.out.println("Fin de la lecture.");
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		}.start();
 	}
 	
 	public static void saveSettings() {
-		System.out.println("Enregistrement du fichier de configuration...");
-		BufferedWriter bw = null;
-		try {
-			 bw = new BufferedWriter(new FileWriter(fichier));
+		new Thread() {
+			@Override
+			public void run() {
+				System.out.println("Enregistrement du fichier de configuration...");
+				BufferedWriter bw = null;
+				try {
+					 bw = new BufferedWriter(new FileWriter(fichier));
 
-			 /*-------Enregistrement du ping----------*/
-			 bw.write("PING="+PING + "\n");
-			 System.out.print("PING="+PING + "\n");
-			 
-			 /*-------Enregistrement du délai----------*/
-			 bw.write("DELAY="+DELAY + "\n");
-			 System.out.print("DELAY="+DELAY + "\n");
+					 /*-------Enregistrement du ping----------*/
+					 bw.write("PING="+PING + "\n");
+					 System.out.print("PING="+PING + "\n");
+					 
+					 /*-------Enregistrement du délai----------*/
+					 bw.write("DELAY="+DELAY + "\n");
+					 System.out.print("DELAY="+DELAY + "\n");
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				bw.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						bw.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			 System.out.println("Enregistrement terminé");
 			}
-		}
-	 System.out.println("Enregistrement terminé");
+		}.start();
 	}
 
 }
