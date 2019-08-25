a6727c39f932ce10eba5b88155b6789d92b0e8bf
Lightel24 <Lightel24@gmail.com>
2019-08-25 12:17:49 +0200

    Ajustements de l'ui

--- SerialCom/src/ui/Parametres.java
+++ SerialCom/src/ui/Parametres.java
@@ -27,11 +27,15 @@ import java.awt.Dimension;
 import javax.swing.JTextPane;
 import javax.swing.JTextField;
 import java.awt.ComponentOrientation;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import java.awt.event.KeyEvent;
+import java.awt.Cursor;
+import java.awt.Font;
 
 public class Parametres extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField txtDelay;
 	/**
 	 * Create the dialog.
 	 * @param ref 
@@ -44,23 +48,29 @@ public class Parametres extends JDialog {
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(new GridLayout(0, 2, 0, 0));
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel EnTete = new JPanel();
+		contentPanel.add(EnTete, BorderLayout.NORTH);
+		EnTete.setBackground(Color.GRAY);
+		
+		JLabel lblParametres = new JLabel("Parametres");
+		lblParametres.setFont(new Font("Verdana", Font.BOLD, 13));
+		lblParametres.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		lblParametres.setForeground(Color.WHITE);
+		EnTete.add(lblParametres);
+		
+		JPanel Content = new JPanel();
+		contentPanel.add(Content, BorderLayout.CENTER);
+		Content.setLayout(new GridLayout(1, 0, 0, 0));
 		
 		JPanel Gauche = new JPanel();
+		Content.add(Gauche);
 		Gauche.setBackground(Color.DARK_GRAY);
 		Gauche.setBorder(new LineBorder(new Color(0, 0, 0)));
-		contentPanel.add(Gauche);
 		Gauche.setLayout(new BorderLayout(0, 0));
-			
-		JPanel EnTete = new JPanel();
-		EnTete.setBackground(Color.GRAY);
-		Gauche.add(EnTete, BorderLayout.NORTH);
-				
-		JLabel lblNewLabel = new JLabel("Socket");
-		lblNewLabel.setForeground(Color.WHITE);
-		EnTete.add(lblNewLabel);
-			
-			
+		
+		
 		JPanel Liste = new JPanel();
 		Liste.setBackground(Color.DARK_GRAY);
 		Gauche.add(Liste, BorderLayout.CENTER);
@@ -82,20 +92,25 @@ public class Parametres extends JDialog {
 		horizontalBox.setAlignmentX(Component.LEFT_ALIGNMENT);
 		Liste.add(horizontalBox);
 		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GRAY);
+		horizontalBox.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("PING");
+		panel.add(chckbxNewCheckBox);
 		chckbxNewCheckBox.setBorder(new EmptyBorder(5, 5, 5, 5));
 		chckbxNewCheckBox.setForeground(Color.WHITE);
-		chckbxNewCheckBox.setBackground(Color.DARK_GRAY);
+		chckbxNewCheckBox.setBackground(Color.GRAY);
 		chckbxNewCheckBox.setSelected(Settings.isPing());
 		chckbxNewCheckBox.setAlignmentX(Component.CENTER_ALIGNMENT);
-		horizontalBox.add(chckbxNewCheckBox);
 		
 		JTextPane txtpnCetteOptionEnvoie = new JTextPane();
+		panel.add(txtpnCetteOptionEnvoie);
 		txtpnCetteOptionEnvoie.setForeground(Color.WHITE);
-		txtpnCetteOptionEnvoie.setBackground(Color.DARK_GRAY);
+		txtpnCetteOptionEnvoie.setBackground(Color.GRAY);
 		txtpnCetteOptionEnvoie.setMaximumSize(new Dimension(2147483647, 80));
 		txtpnCetteOptionEnvoie.setText("Cette option permet de detecter une erreur de communication. A n'utiliser que lorsque connecté à un autre MultiCom.");
-		horizontalBox.add(txtpnCetteOptionEnvoie);
 		
 		Component verticalGlue_1 = Box.createVerticalGlue();
 		verticalGlue_1.setMaximumSize(new Dimension(0, 15));
@@ -112,34 +127,28 @@ public class Parametres extends JDialog {
 		horizontalBox_1.setBackground(Color.DARK_GRAY);
 		horizontalBox_1.setAlignmentX(0.0f);
 		
-		txtDelay = new JTextField();
-		txtDelay.setText(String.valueOf(Settings.getDelay()));
-		txtDelay.setMaximumSize(new Dimension(2147483647, 25));
-		txtDelay.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		txtDelay.setColumns(10);
-		txtDelay.setMinimumSize(new Dimension(20, 2));
-		txtDelay.setBorder(new EmptyBorder(5, 0, 5, 0));
-		horizontalBox_1.add(txtDelay);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.GRAY);
+		horizontalBox_1.add(panel_1);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.LINE_AXIS));
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setMinimumSize(new Dimension(100, 20));
+		spinner.setModel(new SpinnerNumberModel(Settings.getDelay(), new Integer(0), null, new Integer(5)));
+		spinner.setMaximumSize(new Dimension(32767, 30));
+		panel_1.add(spinner);
 		
 		JTextPane textPane = new JTextPane();
+		panel_1.add(textPane);
 		textPane.setText("Délai entre chaque actualisation de la connexion. (ms)");
 		textPane.setMaximumSize(new Dimension(2147483647, 50));
 		textPane.setForeground(Color.WHITE);
-		textPane.setBackground(Color.DARK_GRAY);
-		horizontalBox_1.add(textPane);
-			
+		textPane.setBackground(Color.GRAY);
+		
 		JPanel Droite = new JPanel();
+		Content.add(Droite);
 		Droite.setBorder(new LineBorder(new Color(0, 0, 0)));
-		contentPanel.add(Droite);
 		Droite.setLayout(new BorderLayout(0, 0));
-
-		JPanel EnTete1 = new JPanel();
-		EnTete1.setBackground(Color.GRAY);
-		Droite.add(EnTete1, BorderLayout.NORTH);
-
-		JLabel lblNewLabel1 = new JLabel("Serial");
-		lblNewLabel1.setForeground(Color.WHITE);
-		EnTete1.add(lblNewLabel1);
 		
 		JPanel Liste1 = new JPanel();
 		Liste1.setBackground(Color.DARK_GRAY);
@@ -157,11 +166,7 @@ public class Parametres extends JDialog {
 		okButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Settings.setPing(chckbxNewCheckBox.isSelected());
-				try {
-				Settings.setDelay(Integer.parseInt(txtDelay.getText()));
-				}catch(NumberFormatException e) {
-					e.printStackTrace();
-				}
+				Settings.setDelay((int)spinner.getValue());
 				Settings.saveSettings();
 				Parametres.this.dispose();
 			}
